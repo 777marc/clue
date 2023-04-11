beb1d1dc8e5201e3c1f521ce1f70ca0da5a1253a
Marc Mendoza <777marc@gmail.com>
2023-04-11 19:26:46 -0400

    api

--- lib/clue-stack.ts
+++ lib/clue-stack.ts
@@ -1,10 +1,30 @@
 import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class ClueStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const api = new apigateway.RestApi(this, "api", {
+      description: "example api gateway",
+      deployOptions: {
+        stageName: "dev",
+      },
+      // 👇 enable CORS
+      defaultCorsPreflightOptions: {
+        allowHeaders: [
+          "Content-Type",
+          "X-Amz-Date",
+          "Authorization",
+          "X-Api-Key",
+        ],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: ["*"],
+      },
+    });
 
     const lambaFunction = new Function(this, "lambda-function", {
       functionName: "heyo-cdk",
@@ -13,5 +33,12 @@ export class ClueStack extends cdk.Stack {
       code: Code.fromAsset("lambda"),
       handler: "game-controller.handler",
     });
+
+    const gameItems = api.root.addResource("gameItems");
+
+    gameItems.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(lambaFunction, { proxy: true })
+    );
   }
 }
